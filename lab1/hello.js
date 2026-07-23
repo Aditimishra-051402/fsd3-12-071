41d570f0303d7d114391409321bbf2003573f6a5
Aditimishra-051402 <amaditimishra55@gmail.com>
2026-07-23 11:59:30 +0530

    initial commit

---
+++ lab1/hello.js
@@ -0,0 +1,12 @@
+console.log("Aditi Mishra");
+// write a function to take two numbers as input and return their sum
+function sum(a, b) {
+    return a + b;
+}
+const add=(a,b)=> {
+    return a + b;
+};
+const add2= (a,b)=> a + b;
+ console.log(sum(5, 10));
+ console.log(add(5, 10));
+ console.log(add2(5, 10));
