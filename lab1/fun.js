34718ef9f5a130c9a39aae54583749bc24c36ccb
Aditimishra-051402 <amaditimishra55@gmail.com>
2026-07-23 12:20:46 +0530

    1st commit

--- lab1/fun.js
+++ lab1/fun.js
@@ -1,3 +1,8 @@
 function add(a, b) {
     return a + b;
-}
+} 
+// write a function to take any digit (0-9) and return it in words
+function digitToWords(digit) {
+    const words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    return words[digit];
+} 
