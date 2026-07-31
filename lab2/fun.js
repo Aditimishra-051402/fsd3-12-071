8b9b20e4fd159d1d2631fb08a0bcbc4db4ea98fa
Aditimishra-051402 <amaditimishra55@gmail.com>
2026-07-31 15:19:46 +0530

    lab2 completed

--- lab2/fun.js
+++ lab2/fun.js
@@ -1,16 +1,20 @@
 const f1 = () => {
-    console.log('f1');
+  console.log("f1");
 };
 const f2 = () => {
-    console.log('f2');
+  console.log("f2");
 };
 const f3 = () => {
-    console.log('f3');
+  console.log("f3");
+};
+function main() {
+  console.log("main");
+  setTimeout(f1, 0);
+  //setInterval(f1,1000);
+  setImmediate(f2);
+  f3();
+  console.log("end");
 }
-function main(){
-    console.log("main");
-    f1();
-    f2();
-    f3();
-    console.log("main end");
-}
+main();
+// a function not executed immediately but it must be executed after a while it has some status during the execution
+// it may resolve 
---
+++ lab2/readme.md
@@ -0,0 +1,18 @@
+# EventLoop
+js is synchronous and single threaded bydefault
+## there can be async behaviour 
+- with BrowserAPI -
+setTimeout, setInterval,
+setImmediate, nextTick
+- with promises
+- with event handlers
+# CALL BACK
+call back functions=> that pass as argument or the parameter to another function
+  
+Modern javascript is divided into 2 categories
+1. CommonJS (.cjs) -> supports OOPS -> require
+- first priority (nextTick, Promise, setImmediate/ setTimeout)
+
+2. ModuleJS (.mjs) -> follow modular approach -> import
+- priority (Promise, nextTick, setImmediate/ setTimeout)
+
